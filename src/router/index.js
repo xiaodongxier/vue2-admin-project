dac11e4936782168322e92cbb3cb4abbd0f973a2
小东西儿 <xiaodongxier@gmail.com>
2023-05-15 16:25:36 +0800

    解决重复点击路由报错的BUG

--- src/router/index.js
+++ src/router/index.js
@@ -38,3 +38,8 @@ const route = new VueRouter({
 
 export default route
 
+// 解决重复点击路由报错的BUG
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err)
+}
