c6d52ea85ed3f3ff29520281973d72fc70e8417c
小东西儿 <xiaodongxier@gmail.com>
2023-05-19 16:40:06 +0800

    新增/编辑/删除完成

--- src/views/User.vue
+++ src/views/User.vue
@@ -164,8 +164,25 @@ export default {
     // 删除
     handleBtnDel(row) {
       console.log("删除", row)
-      delUser().then(() => {
-      })
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        delUser({id: row.id}).then(() => { 
+          this.getList()
+          this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+        })
+
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
     },
     // handleAdd(){
     //   this.modelType = 0
