1be7b4fa33f7fd21efb75cc60a032309708b1271
小东西儿 <xiaodongxier@gmail.com>
2023-05-22 16:45:45 +0800

    配置build输出文件夹

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+//vue的一些配置 https://cli.vuejs.org/zh/config/#outputdir
 module.exports = {
-  publicPath: '/vue2-admin-project'
+  // 解决打包文件路径问题
+  publicPath: '/vue2-admin-project',
+  // 打包输出目录修改
+  outputDir: 'docs'
 }
